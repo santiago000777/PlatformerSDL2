091c50a3e9b2fafe10b84fc7d5c9cfed5eee72ca
Jakub Veit <jakub@veit.cz>
2024-03-30 11:03:31 +0100

    Oprava uvolnovani pameti

--- PlatformerSDL2/Game.cpp
+++ PlatformerSDL2/Game.cpp
@@ -22,6 +22,7 @@ void TGame::Init(const std::string& windowName, int posX, int posY, int windowWi
 TGame::~TGame() {
 	for (int i = 0; i < objects.size(); i++) {
 		delete objects.at(i);
+		std::cout << "Deleted Object!" << std::endl;
 	}
 }
 
@@ -41,13 +42,16 @@ void TGame::Loop() {
 
 void TGame::AddTexture(TVec2 pos, TVec2 size, const std::string& path, TVec2 fromXY, float percentX, float percentY) {
 	TGameObject* object = new TGameObject(renderer, pos, size, path, fromXY, percentX, percentY);
+	std::cout << "Created Texture!" << std::endl;
 	objects.push_back(object);
 }
 
 void TGame::AddTexture(TVec2 pos, TVec2 size, TColor&& color) {
 	TGameObject* object = new TGameObject(renderer, pos, size, &color);
+	std::cout << "Created Texture!" << std::endl;
 	objects.push_back(object);
 	delete object;
+	std::cout << "Deleted Texture!" << std::endl;
 }
 
 void TGame::SetBackGround(const std::string& BGpath) {
@@ -60,6 +64,7 @@ void TGame::SetBackGround(const std::string& BGpath) {
 
 void TGame::SetBackGround(TColor&& color) {
 	TBackGround* bg = new TBackGround(windowWidth, windowHeight, color);
+	std::cout << "Created Texture!" << std::endl;
 	backGround = *bg;
 
 	for (int i = 0; i < objects.size(); i++) {
@@ -80,15 +85,21 @@ void TGame::Render() {
 		SDL_BlitScaled(objects.at(i)->texture.GetSurface(), objects.at(i)->texture.GetSrcRect(), &this->finalSurface, objects.at(i)->texture.GetDstRect());
 	}
 	finalTexture = SDL_CreateTextureFromSurface(renderer, &this->finalSurface);
+	//SDL_FreeSurface(&this->finalSurface);
+	//std::cout << "Deleted Surface!" << std::endl;
+
 	SDL_RenderClear(renderer);
 	SDL_Rect* rect1 = new SDL_Rect();
+	std::cout << "Created Rect!" << std::endl;
 	rect1->x = 0; 
 	rect1->y = 0; 
 	rect1->w = windowWidth; 
 	rect1->h = windowHeight; 
 	SDL_RenderCopy(renderer, finalTexture, 0, rect1);
+	SDL_DestroyTexture(finalTexture);
 	SDL_RenderPresent(renderer);
 	delete rect1;
+	std::cout << "Deleted Rect!" << std::endl;
 }
 
 void TGame::Posun() {
